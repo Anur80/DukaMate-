d6db92a9d7ca124200fb1286fd29819ee503040f
Anur80 <anurmayiik99@gmail.com>
2025-06-16 16:55:33 +0300

    First project launched

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: { primary: "#4F46E5", secondary: "#10B981" },
+      borderRadius: {
+        button: "8px",
+      },
+    },
+  },
+  plugins: [],
+};
